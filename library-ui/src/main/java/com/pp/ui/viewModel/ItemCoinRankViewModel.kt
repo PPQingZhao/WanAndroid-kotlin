74285bba45dacc24bf62d035c8bdcf578f7c961d
PPQingZhao <2938672226@qq.com>
2023-09-21 03:55:24 +0800

    积分排行榜优化

--- library-ui/src/main/java/com/pp/ui/viewModel/ItemCoinRankViewModel.kt
+++ library-ui/src/main/java/com/pp/ui/viewModel/ItemCoinRankViewModel.kt
@@ -1,5 +1,11 @@
 package com.pp.ui.viewModel
 
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.graphics.LinearGradient
+import android.graphics.Shader
+import android.graphics.Typeface
+import android.widget.TextView
 import androidx.databinding.ObservableField
 import com.pp.theme.AppDynamicTheme
 
@@ -10,5 +16,63 @@ open class ItemCoinRankViewModel<Data : Any>(
     val username = ObservableField<String>()
     val coinCount = ObservableField<String>()
 
+}
 
+@androidx.databinding.BindingAdapter("android:rankText")
+fun rankText(
+    textView: TextView,
+    itemCoinRankViewModel: ItemCoinRankViewModel<*>,
+) {
+    val rank = itemCoinRankViewModel.rank.get()
+
+    textView.text = rank
+    setLinearGradient(textView, rank, itemCoinRankViewModel.theme.textColorSecondary.value!!)
+}
+
+@androidx.databinding.BindingAdapter("android:userNameText")
+fun userNameText(
+    textView: TextView,
+    itemCoinRankViewModel: ItemCoinRankViewModel<*>,
+) {
+    val username = itemCoinRankViewModel.username.get()
+    val rank = itemCoinRankViewModel.rank.get()
+    textView.text = username
+    setLinearGradient(textView, rank, itemCoinRankViewModel.theme.textColor.value!!)
+}
+
+fun setLinearGradient(
+    textView: TextView,
+    rank: String?,
+    defaultColor: ColorStateList,
+) {
+    var startColor = Color.BLACK
+    var endColor = Color.BLACK
+    if (rank == "1") {
+        textView.textSize = 18f
+        textView.setTypeface(Typeface.SANS_SERIF, Typeface.BOLD_ITALIC)
+        startColor = Color.parseColor("#FFFF00")
+        endColor = Color.parseColor("#EEC591")
+    } else if (rank == "2" || rank == "3") {
+        textView.textSize = 16f
+        textView.setTypeface(Typeface.SANS_SERIF, Typeface.BOLD_ITALIC)
+        startColor = Color.parseColor("#EEC591")
+        endColor = Color.parseColor("#8B7E66")
+    } else {
+        textView.textSize = 13f
+        textView.setTypeface(Typeface.SANS_SERIF, Typeface.NORMAL)
+        startColor = defaultColor.defaultColor
+        endColor = defaultColor.defaultColor
+    }
+
+    val linearGradient = LinearGradient(
+        0f,
+        0f,
+        0f,
+        textView.paint.textSize,
+        startColor,
+        endColor,
+        Shader.TileMode.CLAMP
+    )
+    textView.paint.shader = linearGradient
+    textView.invalidate()
 }
